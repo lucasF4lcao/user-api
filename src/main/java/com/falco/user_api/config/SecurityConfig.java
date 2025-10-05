06f2687b05202b3c4aecfd32e2af101b936b57fa
Lucas Falcão <lucas.olifalco@gmail.com>
2025-10-05 02:18:54 -0300

    Update SecurityConfig.java

--- src/main/java/com/falco/user_api/config/SecurityConfig.java
+++ src/main/java/com/falco/user_api/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://user-front-production-5bb6.up.railway.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
